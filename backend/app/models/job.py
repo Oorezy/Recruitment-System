5cfbb0a93291b292ddc5a899571817e1416bbf96
Ooreoluwa Ibitowa <ooreoluwaibitowa@gmail.com>
2026-03-25 17:44:01 +0000

    I added the Job Schema used to design the database

---
+++ backend/app/models/job.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+
+class Job(SQLModel, table=True):
+    __tablename__ = "jobs"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    title: str
+    description: str
+    department: str
+    location: str
+    job_type: str
+    deadline: str
+    status: str = "Open"
+    required_skills: str 
+    responsibilities: str
+    qualifications: str
+    created_at: str
