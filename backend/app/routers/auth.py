cf0b886e6ef465ada9cb88035bc470359a6ef98f
Ooreoluwa Ibitowa <ooreoluwaibitowa@gmail.com>
2026-03-25 18:53:28 +0000

    I added the registration endpoint and the user DTOs

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,4 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from security import hash_password
+from db.session import get_session
+from schemas.user import UserCreate, UserLogin
+from sqlmodel import Session
+from models.user import User
 
 router = APIRouter()
 
@@ -6,3 +12,26 @@ router = APIRouter()
 @router.get("/auth")
 def auth():
     return {"message": "Auth router working"}
+
+@router.post("/auth/register")
+def register(formData: UserCreate, session: Session = Depends(get_session)):
+    
+    new_user = User(
+        full_name=formData.full_name,
+        email=formData.email,
+        password_hash=hash_password(formData.password),
+        role=formData.role,
+        phone=formData.phone
+    )
+
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user)
+
+    return {"message": "User registered successfully"}
+
+
+@router.post("/auth/login")
+def login(form_data: UserLogin, session: Session = Depends(get_session)):
+    return {"message": "Login endpoint working"}
+
---
+++ backend/app/schemas/user.py
@@ -0,0 +1,26 @@
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+from enums import UserRole
+
+
+class UserCreate(BaseModel):
+    full_name: str
+    email: EmailStr
+    password: str
+    role: UserRole
+    phone: Optional[str] = None
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    full_name: str
+    email: EmailStr
+    role: UserRole
+
+    class Config:
+        from_attributes = True
