68dcd4c045ff489da08d92fa61a0df43eb741191
Ooreoluwa Ibitowa <ooreoluwaibitowa@gmail.com>
2026-03-25 17:43:37 +0000

    I created the User Schema

---
+++ backend/app/models/user.py
@@ -0,0 +1,17 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+from app.enums import UserRole
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    first_name: str
+    last_name: str
+    email: str = Field(unique=True)
+    password_hash: str
+    role: UserRole
+    phone: Optional[str] = None
+    resume_path: Optional[str] = None
+    resume_filename: Optional[str] = None
