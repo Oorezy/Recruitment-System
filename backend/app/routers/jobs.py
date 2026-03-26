6e71c16fdf1b6b986a27095da391df43e3e78957
Ooreoluwa Ibitowa <ooreoluwaibitowa@gmail.com>
2026-03-26 20:11:47 +0000

    Added the right response for the 2 jobs endpoints

--- backend/app/routers/jobs.py
+++ backend/app/routers/jobs.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 
+from utils import serialize_job
 from db.session import get_session
 from models.job import Job
 from schemas.job import JobResponse
@@ -10,8 +11,8 @@ router = APIRouter()
 @router.get("")
 def get_jobs(session: Session = Depends(get_session)):
     jobs = session.exec(select(Job)).all()
-    return JobResponse()
-
+    return [serialize_job(job) for job in jobs]
+    
 
 @router.get("/{job_id}")
 def get_job(job_id: int, session: Session = Depends(get_session)):
@@ -20,4 +21,4 @@ def get_job(job_id: int, session: Session = Depends(get_session)):
     if not job:
         raise HTTPException(status_code=404, detail="Job not found")
 
-    return JobResponse()
+    return serialize_job(job)   
