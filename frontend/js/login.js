4f4bb680a31bf927b33797ba98b139f89a16f3be
Ooreoluwa Ibitowa <ooreoluwaibitowa@gmail.com>
2026-03-30 20:05:58 +0100

    I wrote the login js for signing in

---
+++ frontend/js/login.js
@@ -0,0 +1,32 @@
+const loginForm = document.getElementById("loginForm");
+const messageEl = document.getElementById("message");
+
+loginForm.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    messageEl.textContent = "";
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    try {
+      const response = await apiRequest("/auth/login", "POST", {
+        email,
+        password
+      });
+
+      localStorage.setItem("id", response.id);
+      localStorage.setItem("role", response.role);
+
+      if (response.role === "recruiter") {
+        window.location.href = "recruiter-dashboard.html";
+      } else {
+        window.location.href = "applicant-dashboard.html";
+      }
+    } catch (error) {
+      messageEl.style.color = "red";
+      messageEl.textContent = "Invalid email or password";
+    }
+  });
+
+  
