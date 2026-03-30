de8aba676c88fd9cf63f8cdda665f04a2a620942
Ooreoluwa Ibitowa <ooreoluwaibitowa@gmail.com>
2026-03-30 20:05:07 +0100

    I started writing the javascripts, started with a base API js and also the register js to use to register users

---
+++ frontend/js/register.js
@@ -0,0 +1,42 @@
+const registerForm = document.getElementById("registerForm");
+const registerMessageEl = document.getElementById("message");
+
+registerForm.addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  registerMessageEl.textContent = "";
+
+  const firstName = document.getElementById("firstName").value.trim();
+  const lastName = document.getElementById("lastName").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const role = document.getElementById("role").value;
+  const phone= document.getElementById("phoneNumber").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const confirmPassword = document.getElementById("confirmPassword").value.trim();
+
+  if (password !== confirmPassword) {
+    registerMessageEl.textContent = "Passwords do not match";
+    return;
+  }
+
+  try {
+    const response = await apiRequest("/auth/register", "POST", {
+      first_name: firstName,
+      last_name: lastName,
+      email,
+      phone,
+      password,
+      role
+    });
+
+    registerMessageEl.style.color = "green";
+    registerMessageEl.textContent = response.message || "Registration successful. Redirecting...";
+
+    setTimeout(() => {
+      window.location.href = "login.html";
+    }, 1200);
+  } catch (error) {
+    registerMessageEl.style.color = "red";
+    registerMessageEl.textContent = error.message;
+  }
+});
